d54813c5fa98ac96e9124841329f9a2b5194bf5d
aung306 <92897562+aung306@users.noreply.github.com>
2022-12-06 01:23:10 -0500

    Result page and input handling

--- app.js
+++ app.js
@@ -3,14 +3,10 @@
 const switcher = document.querySelector('.btn');
 const search1 = document.querySelector('.bpdp');
 const search2 = document.querySelector('.filter');
+const backBut = document.querySelector('.bck');
 
 const in1 = document.querySelector('.input1');
 const in2 = document.querySelector('.input2');
-
-var charInput = document.getElementsByName('characters')[0].value;
-var keyInput = document.getElementsByName('keywords')[0].value;
-
-const goBack = document.querySelector('.back');
 
 switcher.addEventListener('click', function() {
     const className = document.body.className;
@@ -22,23 +18,13 @@ switcher.addEventListener('click', function() {
     console.log('current class name: ' + className);
 
     //if enter button is clicked, store the information.
-
+    var charInput = document.getElementsByName('characters')[0].value;
+    var keyInput = document.getElementsByName('keywords')[0].value;
     //now move onto the next page
     location.href="result.html";
     //clear everything afterwards
     document.getElementsByName('characters')[0].value = "";
     document.getElementsByName('keywords')[0].value = "";
-});
-goBack.addEventListener('click', function() {
-    const className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Back";
-    } else {
-        this.textContent = "Enter";
-    }
-    console.log('current class name: ' + className);
-
-    location.href="index.html";
 });
 
 search1.addEventListener('click', function() {
@@ -69,4 +55,15 @@ in2.addEventListener('click', function() {
 
     const className = document.body.className;
     console.log('current class name: ' + className);
+});
+
+backBut.addEventListener('click', function() {
+    const className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Enter";
+    } else {
+        this.textContent = "Back";
+    }
+    console.log('current class name: ' + className);
+    location.href="index.html";
 });
