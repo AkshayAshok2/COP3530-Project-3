80eaab5627ba17b2093cab262b293ee4f1693811
Nic Butakow <nicholas@adastralab.com>
2022-12-06 17:18:01 -0500

    Fix Filter Search and search output

--- search.py
+++ search.py
@@ -25,9 +25,10 @@ with open("data.csv", encoding="ansi") as f:
 text_length = len(text)
 
 def preprocess(pattern):
-    """Outputs a pattern's list of words in lowercase and its length."""
+    """Outputs a pattern's list of words in lowercase, its length, and initial search values."""
     pattern = re.sub(r"[\s]+", " ", re.sub(r"[^\w\s]", "", pattern)).lower().strip().split()
-    return pattern, len(pattern)
+    pattern_length = len(pattern)
+    return pattern, pattern_length, (0, 0, (1 << pattern_length) - 1, 0, 0, pattern_length)
 
 def process(pattern, pattern_length):
     """Outputs an index that stores the match vector for each word in a pattern."""
@@ -35,33 +36,25 @@ def process(pattern, pattern_length):
     for i in range(pattern_length):
         word = pattern[i]
         ith_bit = 1 << i
-        found = False
-        for key in index:
-            if key == word:
-                index[word] |= ith_bit
-                found = True
-                break
-        if not found:
+        if word in index:
+            index[word] |= ith_bit
+        else:
             index[word] = ith_bit
     return index
 
 def get_mask(index, word):
     """Outputs the match vector for a word."""
-    for key in index:
-        if key == word:
-            return index[word]
+    if word in index:
+        return index[word]
     return 0
 
-def bp_dp_search_helper(index, pattern_length, word_bound, start, end):
+def bp_dp_search_helper(index, parameters, not_zero, search_range, previous_input):
     ranks = {}
-    not_zero = (1 << pattern_length) - 1
-    HP = 0 # horizontal positive
-    HN = 0 # horizontal negative
-    VN = 0 # vertical negative
-    D0 = 0 # diagonal zero
-    VP = not_zero # vertical positive
-    score = pattern_length
-    for j in range(start, end):
+    pattern_length, word_bound = parameters
+    HP, HN, VP, VN, D0, score = previous_input
+    next_input = None
+    saved = False
+    for j in search_range:
         PM = get_mask(index, text[j]) # preprocessed mask
         D0 = ((((PM & VP) + VP) ^ VP) | PM | VN) & not_zero
         HP = (VN | ~(D0 | VP)) & not_zero
@@ -69,29 +62,30 @@ def bp_dp_search_helper(index, pattern_length, word_bound, start, end):
         VP = ((HN << 1) | ~(D0 | (HP << 1))) & not_zero
         VN = (HP << 1) & D0
         score += ((HP >> pattern_length - 1) & 1) - ((HN >> pattern_length - 1) & 1)
-        rank = word_bound - score
-        if rank >= 0:
-            rank += 1
+        if not saved:
+            next_input = (HP, HN, VP, VN, D0, score)
+            saved = True
+        rank = word_bound - score + 1
+        if rank > 0:
             episode = episode_index[j]
-            found = False
-            for key in ranks:
-                if key == episode:
-                    ranks[episode].append((j, rank))
-                    found = True
-                    break
-            if not found:
+            if episode in ranks:
+                ranks[episode].append((j, rank))
+            else:
                 ranks[episode] = [(j, rank)]
-    return ranks
+    return ranks, next_input
 
-def bp_dp_search(pattern, word_bound, start=0, end=text_length):
-    pattern, length = preprocess(pattern)
-    return bp_dp_search_helper(process(pattern, length), length, word_bound, start, end)
+def bp_dp_search(pattern, word_bound, search_range=range(0, text_length)):
+    pattern, length, previous_input = preprocess(pattern)
+    ranks, _ = bp_dp_search_helper(process(pattern, length), (length, word_bound),
+            previous_input[2], search_range, previous_input)
+    return ranks
 
 # TODO: Optimize
 def filter_search(pattern, word_bound):
     ranks = {}
-    pattern, pattern_length = preprocess(pattern)
+    pattern, pattern_length, initial_input = preprocess(pattern)
     index = process(pattern, pattern_length)
+    previous_input = initial_input
     i = 0
     while i < text_length - pattern_length + word_bound:
         counter = 0
@@ -103,25 +97,23 @@ def filter_search(pattern, word_bound):
                 break
             position -= 1
         if position == 0:
-            search = bp_dp_search_helper(index, pattern_length, word_bound, i,
-                    i + pattern_length - word_bound)
+            search, previous_input = bp_dp_search_helper(index, (pattern_length, word_bound),
+                    (1 << pattern_length) - 1, range(i, i + pattern_length - word_bound),
+                    previous_input)
             for result in search:
-                found = False
-                for rank in ranks:
-                    if rank == result:
-                        ranks[result] += search[result]
-                        found = True
-                        break
-                if not found:
+                if result in ranks:
+                    ranks[result] += search[result]
+                else:
                     ranks[result] = search[result]
             i += 1
         else:
             i += position
+            previous_input = initial_input
     return ranks
 
-def key(item_set):
+def sum_ranks(ranks):
     result = 0
-    for j, rank in item_set:
+    for j, rank in ranks:
         result -= rank
     return result
 
@@ -131,5 +123,11 @@ def search(pattern, characters, word_bound, use_filter):
         ranks = filter_search(pattern, word_bound)
     else:
         ranks = bp_dp_search(pattern, word_bound)
-    return ranks
-    #return [episode[k] for k, v in sorted(ranks.items(), key=lambda item: key(set(item[1])))]
+    codes = []
+    titles = []
+    descriptions = []
+    for ep in [episode[k] for k, v in sorted(ranks.items(), key=lambda item: sum_ranks(item[1]))]:
+        codes.append(ep[0])
+        titles.append(ep[1])
+        descriptions.append(ep[2])
+    return codes, titles, descriptions
